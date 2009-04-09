3d8ac3a6811d202d1488eb21d018759bcfa3ea4c
Marcelo Giorgi <marcelo.giorgi.martinez@gmail.com>
2009-04-09 11:00:12 -0300

    Link to materials in homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
   map.root :controller => 'home', :action => 'index'
 
+  map.resources :users
+  map.resource  :session
+  map.signup '/signup', :controller => 'users', :action => 'new'
+  map.login  '/login', :controller => 'sessions', :action => 'new'
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+
   map.namespace :admin do |admin|
     #admin.resources :attachments
     admin.resources :users
