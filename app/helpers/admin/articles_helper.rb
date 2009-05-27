bda8c2dfe26f3af196e2b2d38933a51b1d9553f0
Marcelo Giorgi <marcelo.giorgi.martinez@gmail.com>
2009-05-27 00:30:14 -0300

    Ingresar/Salir link only for reference materials

--- app/helpers/admin/articles_helper.rb
+++ app/helpers/admin/articles_helper.rb
@@ -2,7 +2,6 @@ module Admin::ArticlesHelper
   #description field is shown in FCK editor format
   def body_form_column(record, input_name)
     fckeditor_textarea( :record, :body, :toolbarSet => 'Default', :name => input_name, :width => "1000px", :height => "500px" )
-    #text_area :record, :content
   end
   #description text is displayed in rich format
   def body_column(record)
