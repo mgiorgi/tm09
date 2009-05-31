6cac2ab2302c4b09dd8c5886bc75b3cd977b3962
Marcelo Giorgi <marcelo.giorgi.martinez@gmail.com>
2009-05-31 13:18:15 -0300

    Changing menu styles

--- app/controllers/contact_us_controller.rb
+++ app/controllers/contact_us_controller.rb
@@ -2,7 +2,7 @@ class ContactUsController < ApplicationController
   layout 'enter'
   def index
     article = Article.find_by_title(Article::Section::CONTACT_US)
-    @content = article ? CGI::unescape(article.content) : ''
+    @content = article && !article.content.blank? ? CGI::unescape(article.content) : ''
     render :partial => 'articles/show', :layout => 'enter'
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   layout 'enter'
   def index
     article = Article.find_by_title(Article::Section::INICIO)
-    @content = article ? CGI::unescape(article.content) : ''
+    @content = article && !article.content.blank? ? CGI::unescape(article.content) : ''
     render :partial => 'articles/show', :layout => 'enter'
   end
   def public_reference_materials
