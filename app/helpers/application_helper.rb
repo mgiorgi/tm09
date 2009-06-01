f3e5bcbb6679ac2b1ca91cdb8e07ece8e342dab6
Marcelo Giorgi <marcelo.giorgi.martinez@gmail.com>
2009-06-01 14:07:56 -0300

    Bar for menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  MENU_BACKGROUND = {
+    :home => '#FDF1EC',
+    :groups => '#FFFDE4',
+    :reference_materials => '#EEF5FD',
+    :about_us => '#E2ECCB',
+    :contact_us => '#E2ECCB'
+  }
   def google_analytics
     render :partial => 'shared/google_analytics'
   end
