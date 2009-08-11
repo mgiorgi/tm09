879361fc8901b7f0aaaa9c4dad33e95cad0db4d7
Marcelo Giorgi <marcelo.giorgi.martinez@gmail.com>
2009-08-10 21:53:31 -0300

    Changing default article for group landscape

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,7 +2,7 @@ class GroupsController < ApplicationController
   layout 'enter'
   caches_page :index
   def index
-    article = Article.find_by_title(Article::Section::TALLERES_INICIO)
+    article = Article.find_by_title(Article::Section::ESTIMULACION_MEMORIA)
     @content = article && !article.content.blank? ? article.final_content : ''
     if @content.blank?
       render :partial => 'shared/under_construction', :layout => 'enter'
